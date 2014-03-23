4623579ac9f280ebe77289cbb89bccbf02c8095b
obeza <o.bezaudin@gmail.com>
2014-03-23 07:29:06 -0700

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var w3cjs = require('gulp-w3cjs');
+var compass = require('gulp-compass');
+var notify = require("gulp-notify");
+var es = require('event-stream');
+plumber = require('gulp-plumber');
+var failed = false;
+
+gulp.task('browser-sync', function() {  
+    return browserSync.init(["app/css/*.css", "app/js/*.js", "app/*.html"], {
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('w3cjs', function () {
+     gulp.src('app/index.html')
+     .pipe(w3cjs())
+		.pipe(es.map(function (file, cb) {
+			cb(null, file);
+			
+				failed = !file.w3cjs.success;
+		}))
+		
+		.on('end', function () {
+			if (failed) {
+				gulp.src('').pipe(notify({ message : 'Probleme dans l\'HTML'}));
+			}
+		});
+
+});
+
+gulp.task('compass', function () {  
+    return gulp.src('app/scss/style.scss')
+    	.pipe(plumber())
+        .pipe(compass({css : 'app/css',sass : 'app/scss'}))
+        .pipe(gulp.dest('app/css'));
+});
+
+// default gulp task
+gulp.task('default', ['compass', 'browser-sync', 'w3cjs'], function () {  
+    gulp.watch("app/scss/*.scss", ['compass']);
+    gulp.watch("app/index.html", ['w3cjs']);
+});
